cd2fa6bc3823e52bb4633744d07e917d98f75cec
daniel kelly <danieljkelly91@gmail.com>
2018-04-25 20:35:09 +0100

    adds categories into app.js, created AppContainer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import AppContainer from './containers/AppContainer';
 
 class App extends Component {
   render() {
+
+    const categories = [
+      {name: "All", url: "http://hp-api.herokuapp.com/api/characters"},
+      {name: "Students", url: "http://hp-api.herokuapp.com/api/characters/students"},
+      {name: "Staff", url: "http://hp-api.herokuapp.com/api/characters/staff"},
+      {name: "slytherin", url: "http://hp-api.herokuapp.com/api/characters/house/slytherin"}
+      {name: "ravenclaw", url: "http://hp-api.herokuapp.com/api/characters/house/ravenclaw"}
+      {name: "hufflepuff", url: "http://hp-api.herokuapp.com/api/characters/house/hufflepuff"}
+      {name: "gryffindor", url: "http://hp-api.herokuapp.com/api/characters/house/gryffindor"}
+    ]
     return (
-    <h1>hello world</h1>
+    <AppContainer />
     );
   }
 }
