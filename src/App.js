2939587b90851aaa4ac7e1abbb37347d4e1a3d00
daniel kelly <danieljkelly91@gmail.com>
2018-04-25 22:32:33 +0100

    adds dropdown

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     const categories = [
       {name: "All", url: "http://hp-api.herokuapp.com/api/characters"},
       {name: "Students", url: "http://hp-api.herokuapp.com/api/characters/students"},
-      {name: "Staff", url: "http://hp-api.herokuapp.com/api/characters/staff"}
-      // {name: "slytherin", url: "http://hp-api.herokuapp.com/api/characters/house/slytherin"},
-      // {name: "ravenclaw", url: "http://hp-api.herokuapp.com/api/characters/house/ravenclaw"},
-      // {name: "hufflepuff", url: "http://hp-api.herokuapp.com/api/characters/house/hufflepuff"},
-      // {name: "gryffindor", url: "http://hp-api.herokuapp.com/api/characters/house/gryffindor"}
+      {name: "Staff", url: "http://hp-api.herokuapp.com/api/characters/staff"},
+      {name: "slytherin", url: "http://hp-api.herokuapp.com/api/characters/house/slytherin"},
+      {name: "ravenclaw", url: "http://hp-api.herokuapp.com/api/characters/house/ravenclaw"},
+      {name: "hufflepuff", url: "http://hp-api.herokuapp.com/api/characters/house/hufflepuff"},
+      {name: "gryffindor", url: "http://hp-api.herokuapp.com/api/characters/house/gryffindor"}
     ]
 
     return (
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,11 +4,11 @@ const Heading = (props) => {
   return (
     <div className='heading'>
       {props.title}
-      {/* <select className="dropDown" onChange={props.handleSelectChange}>
-        {props.genres.map(genre => {
-          return <option key={genre.name} value={genre.url}>{genre.name}</option>
+      <select className="dropDown" onChange={props.handleSelectChange}>
+        {props.categories.map(category => {
+          return <option key={category.name} value={category.url}>{category.name}</option>
         })}
-      </select> */}
+      </select>
     </div>
   );
 };
